40278666cc4e19dcfa4f464ea2729a91717306cb
Ankush Gupta <ankushgupta365@gmail.com>
2022-09-30 22:15:46 +0530

    Announcement component added

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+    height: 30px;
+    background-color: teal;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 14px;
+    font-weight: 500;
+`
+
+const Announcement = () => {
+  return (
+    <Container>
+        Get super exciting deals on this diwali
+    </Container>
+  )
+}
+
+export default Announcement

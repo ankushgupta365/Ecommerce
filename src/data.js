46c29118376a17f46813ac2901cb5c4c0e86a333
Ankush Gupta <ankushgupta365@gmail.com>
2022-10-05 01:31:40 +0530

    data modified, broken img links fixed

--- src/data.js
+++ src/data.js
@@ -47,7 +47,7 @@ export const sliderItems = [
     },
     {
       id:2,
-      img:"https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+      img:"https://www.pngarts.com/files/17/Halloween-T-Shirt-Transparent.png",
     },
     {
       id:3,
@@ -67,7 +67,7 @@ export const sliderItems = [
     },
     {
       id:7,
-      img:"https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+      img:"https://www.pngarts.com/files/17/Halloween-T-Shirt-PNG-HQ-Pic.png",
     },
     {
       id:8,
